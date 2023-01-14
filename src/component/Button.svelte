25e3d4ebea27f04b2ea1f57852d7a3e41a75ae12
froyodoc <muhammadkharismam@gmail.com>
2023-01-14 10:38:40 +0800

    initial commit

---
+++ src/component/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    let animation = false;
+</script>
+
+<button class="btn btn-primary bg-blue-600 hover:bg-blue-700 py-1 px-2 text-white {animation === true ? 'animate-wiggle' : '' }"
+    on:click={() => {
+        animation = true
+        setTimeout(() => {
+            animation = false
+        }, 1000)
+    }}
+>
+    Primary
+</button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" }
+        }
+      },
+      animation: {
+        wiggle: "wiggle 200ms ease-in-out"
+      }
+    },
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
