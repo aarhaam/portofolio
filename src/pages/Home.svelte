4217c54c79dfe691c5311a6e319ccc7f6b2c84ce
froyodoc <muhammadkharismam@gmail.com>
2023-01-16 08:27:37 +0800

    animation mobile

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -10,34 +10,60 @@
             Hello, I'm Mahardika....
         </h2>
     </div>
-    <p class="mt-5 text-lg typewriter-section font-normal">Frontend Engineer</p>
+    <p class="mt-1 text-lg typewriter-section font-normal">Frontend Engineer</p>
 </main>
 
 
 <style>
-    .typewriter {
-        overflow: hidden;
-        white-space: nowrap;
-        letter-spacing: 0.2rem;
-        animation:
-            typing 5s steps(100, end),
-            blink-caret 0.1s step-end infinite;
+    @media only screen and (max-width: 512px) {
+        .typewriter {
+            overflow: hidden;
+            white-space: nowrap;
+            animation:
+                    typing 3s steps(100, end),
+                    blink-caret 0.1s step-end infinite;
+        }
+
+        .typewriter-section {
+            overflow: hidden;
+            white-space: nowrap;
+            animation:
+                    typing 3s steps(100, end),
+                    blink-caret 0.1s step-end infinite;
+            animation-delay: 2.5s;
+            animation-fill-mode: both;
+        }
+
+
+        @keyframes typing {
+            from { width: 0 }
+            to { width: 100% }
+        }
     }
 
-    .typewriter-section {
-        overflow: hidden;
-        white-space: nowrap;
-        letter-spacing: 0.2rem;
-        animation:
-                typing 5s steps(100, end),
-                blink-caret 0.1s step-end infinite;
-        animation-delay: 1.5s;
-        animation-fill-mode: both;
-    }
+    @media screen and (min-width: 786px) {
+        .typewriter {
+            overflow: hidden;
+            white-space: nowrap;
+            animation:
+                    typing 5s steps(100, end),
+                    blink-caret 0.1s step-end infinite;
+        }
+
+        .typewriter-section {
+            overflow: hidden;
+            white-space: nowrap;
+            animation:
+                    typing 5s steps(100, end),
+                    blink-caret 0.1s step-end infinite;
+            animation-delay: 1.5s;
+            animation-fill-mode: both;
+        }
 
 
-    @keyframes typing {
-        from { width: 0 }
-        to { width: 100% }
+        @keyframes typing {
+            from { width: 0 }
+            to { width: 100% }
+        }
     }
 </style>
